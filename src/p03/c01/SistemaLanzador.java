e4bdf49b35050c85db839f039f2ec570060fce6b
AnaSalces <anasalcesm@gmail.com>
2022-03-29 09:37:20 +0200

    Ampliación de la clase SistemaLanzador para agregar los hilos de las salidas.
    >> Clase completada total, abierta aún a pequeños cambios de mejora

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,26 +1,26 @@
 package src.p03.c01;
 
 public class SistemaLanzador {
+
 	public static void main(String[] args) {
-		
+
 		IParque parque = new Parque(); // TODO
 		char letra_puerta = 'A';
-		
+
 		System.out.println("Â¡Parque abierto!");
-		
+
 		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
-			// CreaciÃ³n de hilos de entrada
+
+			String puerta = "" + ((char) (letra_puerta++));
+
+			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
+			new Thread(entradas).start();
+
+			// Creación de hilos de salida
+			ActividadEntradaPuerta salidas = new ActividadEntradaPuerta(puerta, parque);
+			new Thread(salidas).start();
+
 		}
-	}	
+	}
 }
