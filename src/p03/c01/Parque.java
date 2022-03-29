126e2fde724d93e1b3cb3dac1ad19efaabcbc87e
AnaSalces <anasalcesm@gmail.com>
2022-03-29 09:44:13 +0200

    Ampliación de las funcionalidades del método salirAlParque. Registrada la puerta de salida, decrementa los contadores de puerta y de personas totales en el parque.

--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -3,108 +3,114 @@ package src.p03.c01;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-public class Parque implements IParque{
-	
-	
+public class Parque implements IParque {
+
 	private int MAX_AFORO = 50;
 	private int MIN_AFORO = 0;
 	private int contadorPersonasTotales;
 	private Hashtable<String, Integer> contadoresPersonasPuerta;
-	
-	
-	public Parque() {	// TODO
+
+	public Parque() { // TODO
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
 		// TODO
 	}
 
-
 	@Override
-	public void entrarAlParque(String puerta){		// TODO
-		
+	public void entrarAlParque(String puerta) { // TODO
+
 		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
+		if (contadoresPersonasPuerta.get(puerta) == null) {
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
-		
-		// TODO
-				
-		
+
+		comprobarAntesDeEntrar();
+
 		// Aumentamos el contador total y el individual
-		contadorPersonasTotales++;		
-		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta)+1);
-		
+		contadorPersonasTotales++;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) + 1);
+
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
-		
+
 		checkInvariante();
-		
-		
+
 		// TODO
-		
+
 	}
-	
 
 	@Override
 	public void salirAlParque(String puerta) {
-		// TODO Auto-generated method stub
-		
+
+		// Es posible que alguna persona salga por una puerta
+		// de la cual nadie haya entrado todavía
+
+		if (contadoresPersonasPuerta.get(puerta) == null) {
+			contadoresPersonasPuerta.put(puerta, 0);
+		}
+
+		comprobarAntesDeSalir();
+
+		// Decrementamos el contador total y el individual
+		contadorPersonasTotales--;
+		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
+
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Salida");
-		
+
 	}
 
-	
-	
-	private void imprimirInfo (String puerta, String movimiento){
+	private void imprimirInfo(String puerta, String movimiento) {
 		System.out.println(movimiento + " por puerta " + puerta);
-		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
-		
+		System.out.println("--> Personas en el parque " + contadorPersonasTotales); // + " tiempo medio de estancia: " +
+																					// tmedio);
+
 		// Iteramos por todas las puertas e imprimimos sus entradas
-		for(String p: contadoresPersonasPuerta.keySet()){
+		for (String p : contadoresPersonasPuerta.keySet()) {
 			System.out.println("----> Por puerta " + p + " " + contadoresPersonasPuerta.get(p));
 		}
 		System.out.println(" ");
 	}
-	
+
 	private int sumarContadoresPuerta() {
 		int sumaContadoresPuerta = 0;
-			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
-			while (iterPuertas.hasMoreElements()) {
-				sumaContadoresPuerta += iterPuertas.nextElement();
-			}
+		Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
+		while (iterPuertas.hasMoreElements()) {
+			sumaContadoresPuerta += iterPuertas.nextElement();
+		}
 		return sumaContadoresPuerta;
 	}
-	
+
 	protected void checkInvariante() {
-		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-		// TODO 
+		assert sumarContadoresPuerta() == contadorPersonasTotales
+				: "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
 		// TODO
-		
-		
-		
+		// TODO
+
 	}
 
-	protected void comprobarAntesDeEntrar(){	// TODO
-		if(contadorPersonasTotales == MAX_AFORO) {
+	protected void comprobarAntesDeEntrar() { // TODO
+		if (contadorPersonasTotales == MAX_AFORO) {
 			try {
 				wait();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
 
-	protected void comprobarAntesDeSalir() throws InterruptedException{		// TODO
-		if(contadorPersonasTotales <= MIN_AFORO) {
-			wait();
-			
+	protected void comprobarAntesDeSalir() { // TODO
+		if (contadorPersonasTotales <= MIN_AFORO) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
-
-
-
 
 }
